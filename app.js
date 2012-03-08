4be4bff9bbc40843f37a171e69244c5a5a624784
Colm Ryan <caryan@veronica-koss-macbook-pro.local>
2012-03-07 22:38:19 -0500

    Major refactoring into the new 2.0 app layout from sencha build command.

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-// Copyright 2012 Colm Ryan colm@colmryan.org
-// License GPL v3 (http://www.gnu.org/licenses/gpl.txt) 
+//<debug>
+Ext.Loader.setPath({
+    'Ext': 'sdk/src'
+});
+//</debug>
 
 Ext.application({
     name: 'CBCRadioPlayer',
+
+    requires: [
+        'Ext.MessageBox'
+    ],
 
     controllers: ['Main'],
     views: ['Main', 'CurrentlyPlaying', 'LocalStationList', 'PodCastList', 'PodCastDetailList'],
     stores: ['LocalStations', 'Favourites', 'PodCasts', 'PodCastDetails'],
     models: ['LocalStation', 'Favourite', 'PodCast', 'PodCastDetail'],
+    icon: {
+        57: 'resources/icons/Icon.png',
+        72: 'resources/icons/Icon~ipad.png',
+        114: 'resources/icons/Icon@2x.png',
+        144: 'resources/icons/Icon~ipad@2x.png'
+    },
+    
+    phoneStartupScreen: 'resources/loading/Homescreen.jpg',
+    tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
 
     launch: function() {
-        Ext.Viewport.add({
-            xtype: 'mainpanel'
-        });
+        // Destroy the #appLoadingIndicator element
+        Ext.fly('appLoadingIndicator').destroy();
+
+        // Initialize the main view
+        Ext.Viewport.add(Ext.create('CBCRadioPlayer.view.Main'));
+    },
+
+    onUpdated: function() {
+        Ext.Msg.confirm(
+            "Application Update",
+            "This application has just successfully been updated to the latest version. Reload now?",
+            function() {
+                window.location.reload();
+            }
+        );
     }
 });
