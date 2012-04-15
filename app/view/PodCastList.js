fea5dae5d7cc34705fcad7fcc6ce9d72a3f998d0
Colm Ryan <cryan@bbn.com>
2012-04-15 16:36:57 -0400

    Moved to a component layout with a custom dataview for nicely displaying the podcastlist with images.

--- app/view/PodCastList.js
+++ app/view/PodCastList.js
@@ -2,29 +2,32 @@
 // License GPL v3 (http://www.gnu.org/licenses/gpl.txt) 
 
 Ext.define('CBCRadioPlayer.view.PodCastList',{
-  extend: 'Ext.List',
+  extend: 'Ext.DataView',
   xtype: 'podcastlist',
-  requires: ['CBCRadioPlayer.store.PodCasts', 'Ext.field.Search'],
+  requires: ['CBCRadioPlayer.store.PodCasts', 'CBCRadioPlayer.view.PodCastListItem', 'Ext.field.Search'],
   
   config: {
   	title: 'PodCasts',
-  	itemTpl: '{title}',
   	store: 'PodCasts',
-  	
-	items: [
-        {
-            xtype: 'toolbar',
-            docked: 'top',
+  	ui: 'podcasts',
+    useComponents: true,
+    defaultType: 'podcastlistitem',
+    deselectOnContainerClick: false,
 
-            items: [
-              	  {
-                    xtype: 'searchfield',
-                    id: 'podcastsearchfield',
-                    placeHolder: 'Search...'
-                	}
-            	]
-    	    }
-	    ]
-	}
+	// items: [
+        // {
+            // xtype: 'toolbar',
+            // docked: 'top',
+// 
+            // items: [
+              	  // {
+                    // xtype: 'searchfield',
+                    // id: 'podcastsearchfield',
+                    // placeHolder: 'Search...'
+                	// }
+            	// ]
+    	    // }
+	    // ]
+	},
 	
 });
