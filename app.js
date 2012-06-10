37086006810e14c356ccdd26ed37fbab0be9a7b8
Colm Ryan <colm@colmryan.org>
2012-06-10 17:06:57 -0400

    Update to latest tools and SDK.
    --CAR

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-//<debug>
-Ext.Loader.setPath({
-    'Ext': 'sdk/src'
-});
-//</debug>
-
 Ext.application({
     name: 'CBCRadioPlayer',
 
@@ -15,15 +9,24 @@ Ext.application({
     views: ['Main', 'CurrentlyPlaying', 'LocalStationList', 'PodCastList', 'PodCastDetailList'],
     stores: ['LocalStations', 'Favourites', 'PodCasts', 'PodCastDetails'],
     models: ['LocalStation', 'Favourite', 'PodCast', 'PodCastDetail'],
+
     icon: {
-        57: 'resources/icons/Icon.png',
-        72: 'resources/icons/Icon~ipad.png',
-        114: 'resources/icons/Icon@2x.png',
-        144: 'resources/icons/Icon~ipad@2x.png'
+        '57': 'resources/icons/Icon.png',
+        '72': 'resources/icons/Icon~ipad.png',
+        '114': 'resources/icons/Icon@2x.png',
+        '144': 'resources/icons/Icon~ipad@2x.png'
     },
-    
-    phoneStartupScreen: 'resources/loading/Homescreen.jpg',
-    tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
+
+    isIconPrecomposed: true,
+
+    startupImage: {
+        '320x460': 'resources/startup/320x460.jpg',
+        '640x920': 'resources/startup/640x920.png',
+        '768x1004': 'resources/startup/768x1004.png',
+        '748x1024': 'resources/startup/748x1024.png',
+        '1536x2008': 'resources/startup/1536x2008.png',
+        '1496x2048': 'resources/startup/1496x2048.png'
+    },
 
     launch: function() {
         // Destroy the #appLoadingIndicator element
@@ -37,8 +40,10 @@ Ext.application({
         Ext.Msg.confirm(
             "Application Update",
             "This application has just successfully been updated to the latest version. Reload now?",
-            function() {
-                window.location.reload();
+            function(buttonId) {
+                if (buttonId === 'yes') {
+                    window.location.reload();
+                }
             }
         );
     }
